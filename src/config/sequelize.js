d925a6caee197c2d15be957083c0992fc175817a
tmfadhlul <tmfadhlul@gmail.com>
2021-10-05 20:48:55 +0700

    test

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,15 +1,21 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT || 'postgres',
-  },
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT || 'postgres',
+//   },
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect:  'postgres',
+  protocol: 'postgres',
+  logging:  true //false
+});
 
 sequelize
   .authenticate()
