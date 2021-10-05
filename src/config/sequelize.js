92f53671bcc92df7cf37111c49b7bf6a6045078f
tmfadhlul <tmfadhlul@gmail.com>
2021-10-05 20:35:40 +0700

    remove lock

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,29 +1,15 @@
 const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,
-
-    {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      port: 5432,
-      host: 'd260lo34p30fvl',
-      logging: true, // false
-    });
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: process.env.DB_DIALECT,
-    },
-  );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+  },
+);
 
 sequelize
   .authenticate()
