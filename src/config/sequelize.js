97c13944968e9561f2fe670c216e40f8762313d6
tmfadhlul <tmfadhlul@gmail.com>
2021-10-05 19:59:27 +0700

    add procfile

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,15 +1,30 @@
-const Sequelize = require('sequelize');
+let Sequelize = require("sequelize"),
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+let sequelize;
+
+if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL,
+
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-  },
-);
+  dialect: "postgres",
+  protocol: "postgres",
+  port: 5432,
+  host: "<heroku host>",
+  logging: true //false
+  });
+
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: process.env.DB_DIALECT,
+    },
+  );
+}
 
 sequelize
   .authenticate()
