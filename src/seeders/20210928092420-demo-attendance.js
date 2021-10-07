5d3e0e11bb73a77aa812fda676dadffaa1b6c6a6
tmfadhlul <tmfadhlul@gmail.com>
2021-10-08 03:43:53 +0700

    fix default value

--- src/seeders/20210928092420-demo-attendance.js
+++ src/seeders/20210928092420-demo-attendance.js
@@ -1,15 +1,18 @@
-'use strict';
+
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
+    const [users] = await queryInterface.sequelize.query(
+      'SELECT id from COURSES;',
+    );
+
+
     return queryInterface.bulkInsert('Attendances', [{
-      
+      UserId: users[0].id,
       createdAt: new Date(),
-      updatedAt: new Date()
+      updatedAt: new Date(),
     }]);
   },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Attendances', null, {});
-  }
+  down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Attendances', null, {}),
 };
