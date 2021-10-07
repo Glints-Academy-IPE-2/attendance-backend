a528c83842d0d7ae47459f4916ddf4e76eb91b09
tmfadhlul <tmfadhlul@gmail.com>
2021-10-08 03:32:42 +0700

    fix default value

--- src/migrations/202110021107145-create-userAttendances.js
+++ src/migrations/202110021107145-create-userAttendances.js
@@ -1,43 +1,40 @@
-'use strict';
+
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('userAttendances', {
-      userId: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-        ref: "User",
-      },
-      token: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        defaultValue: "343512"
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        required: true,
-        ref: "User"
-      },
-      attendance_id: {
-        type: DataTypes.INTEGER,
-        require: true,
-        ref: "Attendance"
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: new Date(),
-        expires: 3600
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: true,
-        defaultValue: new Date(),
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('userAttendances');
-  }
+  up: (queryInterface, DataTypes) => queryInterface.createTable('userAttendances', {
+    userId: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      ref: 'User',
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: '343512',
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      required: true,
+      ref: 'User',
+    },
+    attendance_id: {
+      type: DataTypes.INTEGER,
+      require: true,
+      ref: 'Attendance',
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+      expires: 3600,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: new Date(),
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('userAttendances'),
 };
