325e06ec830a887264e31552e9e34815267d1892
AlviGeo <alvigeovan29@gmail.com>
2021-10-12 16:36:53 +0700

    edit

--- src/seeders/20211003065336-demo-userAttendances.js
+++ src/seeders/20211003065336-demo-userAttendances.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('userAttendances', [{
-      token: '74981974',
+      month: "October",
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
